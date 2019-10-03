176842141af094c7eedf15c225720d22bb873000
Eric Harrison <eric.h@rrison.io>
2019-10-02 21:35:50 -0400

    begin writing update script

--- scripts/update.js
+++ scripts/update.js
@@ -1,1 +1,16 @@
-// TODO: Add update script to take json file and update Firebase
+require('dotenv').config();
+
+const fs = require('fs');
+const fk = require('firebase-key');
+const admin = require('firebase-admin');
+
+const contentPackName = process.argv[2];
+
+if (!contentPackName) {
+  throw 'Please provide contentPack name that matches JSON file';
+}
+
+const fileString = fs.readFileSync(`./contentPacks/${contentPackName}.json`, {
+  encoding: 'utf8',
+});
+console.log(JSON.parse(fileString));
